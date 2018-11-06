cc905ec3e8fa7c67399f806695ec17114b021532
Julien Fleury <julien.fleury@fr.ibm.com>
2018-11-06 17:36:47 +0100

    Aframe action handler

---
+++ public/js/aframe-panel.js
@@ -0,0 +1,174 @@
+// The ConversationPanel module is designed to handle
+// all display and behaviors of the conversation column of the app.
+/* eslint no-unused-vars: "off" */
+/* global Api: true, Common: true*/
+
+var AframePanel = (function () {
+  let settings = {
+    objectsIds: {
+      box: 'aframe-box',
+      sphere: 'aframe-sphere',
+      cylinder: 'aframe-cylinder',
+      sky: 'aframe-sky',
+      plane: 'aframe-plane',
+    },
+    frToEn: {
+      cube: 'box',
+      ciel: 'sky',
+      cylindre: 'cylinder',
+      sol: 'plane',
+      'sphère': 'sphere',
+      blanc: 'white',
+      bleu: 'blue',
+      gris: 'grey',
+      jaune: 'yellow',
+      noir: 'black',
+      orange: 'orange',
+      rouge: 'red',
+      vert: 'green',
+      violet: 'purple',
+    },
+    colors: {
+      white: 'white',
+      blue: '#4CC3D9',
+      grey: '#ECECEC',
+      yellow: '#FFC65D',
+      black: '#333333',
+      orange: 'orange',
+      red: '#EF2D5E',
+      green: '#7BC8A4',
+      purple: 'purple',
+    }
+  };
+  let objects = {};
+
+  // Publicly accessible methods defined
+  return {
+    init,
+  };
+
+  // Initialize the module
+  function init() {
+    chatUpdateSetup();
+    $.each(settings.objectsIds, (obj, id) => {
+      objects[obj] = document.getElementById(id);
+    });
+  }
+
+  // Set up callbacks on payload setters in Api module
+  // This causes the displayMessage function to be called when messages are sent / received
+  function chatUpdateSetup() {
+    var currentResponsePayloadSetter = Api.setResponsePayload;
+    Api.setResponsePayload = function (newPayloadStr) {
+      currentResponsePayloadSetter.call(Api, newPayloadStr);
+      receivedMessage(JSON.parse(newPayloadStr));
+    };
+  }
+
+  function getObj(objNameFr) {
+    return objects[settings.frToEn[objNameFr]];
+  }
+  function getCol(colNameFr) {
+    return settings.colors[settings.frToEn[colNameFr]];
+  }
+
+  function getIntents(payload) {
+    intents = {};
+    payload.intents.forEach(intent => {
+      intents[intent.intent] = intent.confidence;
+    });
+    return intents;
+  }
+  function getEntities(payload) {
+    entities = {};
+    payload.entities.forEach(entity => {
+      switch (entity.entity) {
+        case 'object':
+          if (entity.value == 'tout') {
+            entities[entity.entity] = 'all';
+          } else {
+            entities[entity.entity] = getObj(entity.value);
+          }
+          break;
+        case 'color':
+          entities[entity.entity] = getCol(entity.value);
+          break;
+        default:
+          entities[entity.entity] = entity.value;
+      }
+    });
+    return entities;
+  }
+
+  function applyObject(obj, fn) {
+    if (obj == 'all') {
+      $.each(objects, (objName, object) => {
+        fn(object);
+      });
+    } else {
+      fn(obj);
+    }
+  }
+
+  function colorChange(entities) {
+    if (entities.object && entities.color) {
+      applyObject(entities.object, obj => obj.setAttribute('color', entities.color));
+    } else {
+      console.error("User didn't provide an object and a color for a color change.");
+    }
+  }
+
+  function removeObject(entities) {
+    if (entities.object) {
+      applyObject(entities.object, obj => obj.setAttribute('visible', false));
+    } else {
+      console.error("User didn't provide an object for a object removal.");
+    }
+  }
+
+  function addObject(entities) {
+    if (entities.object) {
+      applyObject(entities.object, obj => {
+        if (entities.color) {
+          obj.setAttribute('color', entities.color);
+        }
+        obj.setAttribute('visible', true);
+      });
+    } else {
+      console.error("User didn't provide an object for a object removal.");
+    }
+  }
+
+  // Function called when the Assistant respond to a text wich has been sent to it.
+  function receivedMessage(payload) {
+    console.log(payload);
+    let intents = getIntents(payload);
+    let entities = getEntities(payload);
+    payload.intents.forEach(intent => {
+      if (intent.confidence > 0) {
+        switch (intent.intent) {
+          case 'order':
+            switch (entities.action) {
+              case 'changer':
+                colorChange(entities);
+                break;
+              case 'ajouter':
+                addObject(entities);
+                break;
+              case 'supprimer':
+                removeObject(entities);
+                break;
+              default:
+                console.error(`Action ${entities.action} not implemented yet`);
+            };
+            break;
+          default:
+            console.error(`Intent ${intent.intent} not implemented yet`);
+        }
+
+      } else {
+        console.log(`Not enough confidence for intent ${intent.intent} (${intent.confidence})`);
+      }
+    });
+  }
+}());
---
+++ public/js/global-aframe.js
@@ -0,0 +1,13 @@
+
+/* global ConversationPanel: true, PayloadPanel: true*/
+/* eslint no-unused-vars: "off" */
+
+// Other JS files required to be loaded first: apis.js, conversation.js, payload.js
+(function () {
+  // Initialize all modules
+  // Blocks: a473b143-b208-433e-99bc-c1ded5b5eceb
+  // Bank:   a4d62b92-693c-4876-a2fb-5389863fd7e6
+  ConversationPanel.init('aab81cb4-8ecf-4273-a85e-6220aec442f9');
+  AframePanel.init();
+  PayloadPanel.init();
+})();
